18e62716399fd7cb0b3029248ef5bb83e234ff4c
CodeAllDayLong <zhouyu.nlp@gmail.com>
2021-05-29 09:06:39 +0100

    add wordament

---
+++ Wordament/wordament.py
@@ -0,0 +1,10 @@
+# This is a script for game 'Wordament'
+
+#--- First Part is to create a new game.
+#--- For each play, the elements include:
+#---     a 4x4 table in which every grid has a letter
+
+def GenerateGame():
+
+    return
+
