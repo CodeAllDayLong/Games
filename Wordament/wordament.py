7f294144d557354952402e9389cce60725686538
JessieZzz <76879261+CodeAllDayLong@users.noreply.github.com>
2021-05-31 14:30:00 +0100

    Update wordament.py
    update wordament

--- Wordament/wordament.py
+++ Wordament/wordament.py
@@ -9,21 +9,47 @@ endict = enchant.Dict("en_UK")
 import random
 import string
 
-def GenerateGame(play=True):
-    
-    # randomly generate letters in grids
+def GenerateGame(letters=False, play=False):
     all_letters = []
-    one_letter = random.sample(string.ascii_uppercase, 1)
-    all_letters.append((one_letter))
-    
-    # show game in terminal if play==True
+    if letters == False:
+        # randomly generate letters in grids
+        for i in range(16):
+            #one_letter = random.sample(string.ascii_uppercase, 1)
+            k = random.randint(0,25)
+            one_letter = string.ascii_uppercase[k]
+            all_letters.append((one_letter))
+        
+
+            
+    if letters:
+        # pre-asign letters
+        # format should be like letters="abcdabcdabcdabcd" length=16
+        if isinstance(letters, str):
+            if len(letters)==16:
+                print("ok self-defined letter grid received.")
+                all_letters = list(letters)
+            else:
+                print("letters should be a string of 16 letters")
+                return
+        else:
+            print("letters should be a string of 16 letters")
+            
+        # show game in terminal if play==True
     if play == True:
         for i in range(16):
             if i != 0 and i%4 == 0:
-                print("\n")
-            print(all_letters[i], " ")
-            
-    # need alphabet and dictionary
-    return
+                print(" ")
+            print(all_letters[i], end=' ')
+        print("\n")
+    return all_letters
 
-GenerateGame()
+a = GenerateGame(letters="LSDIOABCUEMWQLXY",play=True)
+B = GenerateGame(play=True)
+
+def Find_16(all_letters, show=True):
+    answers = []
+    
+    if show==True:
+        for answer in answers:
+            print(answer)
+    return answers
